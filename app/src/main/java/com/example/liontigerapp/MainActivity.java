09c8141ff6801c4190c4bbe6dae9d760258570b2
dhirender04 <kumardhirender04@gmail.com>
2020-09-06 09:47:48 +0530

    two player turn option and add tiger and lion image

--- app/src/main/java/com/example/liontigerapp/MainActivity.java
+++ app/src/main/java/com/example/liontigerapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.liontigerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.gridlayout.widget.GridLayout;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    enum Player{
+        ONE,TWO
+    }
+    Player currentPlayer =Player.ONE;
 
 
 
@@ -27,8 +31,16 @@ public class MainActivity extends AppCompatActivity {
     public void imageViewIsTapped(View imageView){
         ImageView tappedImageView=(ImageView) imageView;
         tappedImageView.setTranslationX(-2000);
-        tappedImageView.setImageResource(R.drawable.lion);
-        tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
 
+        if(currentPlayer==Player.ONE) {
+            tappedImageView.setImageResource(R.drawable.lion);
+            tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
+            currentPlayer=Player.TWO;
+        }else if(currentPlayer==Player.TWO){
+            tappedImageView.setImageResource(R.drawable.tiger);
+            tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
+            currentPlayer=Player.ONE;
+
+        }
     }
 }
